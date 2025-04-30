aae989347303af91962847e721c3a9d3bc711b97
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2025-04-30 20:59:21 +0100

    Updated file structure

---
+++ components/atoms/form/Button.jsx
@@ -0,0 +1,148 @@
+'use client';
+
+import React from 'react';
+import { FaSpinner } from 'react-icons/fa';
+import Ripples from 'react-ripples';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { poppins_500 } from '@/lib/config/font.config';
+
+const Button = ({
+    children,
+    loaderSize = 25,
+    className,
+    flat,
+    to,
+    download,
+    type,
+    wide,
+    outlined,
+    loading,
+    round,
+    onClick,
+    disabled,
+    loaderColor,
+    id,
+    childrenClassName,
+    span,
+    ...props
+}) => {
+    const commonClasses = cn(
+        wide && 'flex-grow w-full',
+        outlined && 'border border-primary',
+        round ? 'rounded-full' : 'rounded-lg',
+        'inline-block py-2 px-4 font-medium flex items-center justify-center cursor-pointer flex-shrink-0 font-nunito font-normal',
+        flat ? 'text-primary bg-white' : 'bg-primary text-white',
+        poppins_500.className,
+        className
+    );
+
+    const commonProps = {
+        className: commonClasses,
+        disabled: disabled || loading,
+        id,
+    };
+
+    if (to) {
+        if (disabled) {
+            return (
+                <span {...props} {...commonProps}>
+                    {children}
+                </span>
+            );
+        } else {
+            return (
+                <Link download={download} href={to} className="overflow-hidden">
+                    <button
+                        type={type}
+                        {...props}
+                        disabled={commonProps.disabled}
+                        id={commonProps.id}
+                        className={cn(wide && 'w-full flex-grow')}
+                    >
+                        <div
+                            id={commonProps.id}
+                            className={cn(
+                                'hover:!shadow-none !shadow-none inline-block',
+                                commonProps.className
+                            )}
+                            onClick={onClick}
+                            itemScope
+                        >
+                            <p
+                                className={cn(
+                                    'text-clash-grotesk font-medium flex items-center justify-center space-x-2',
+                                    childrenClassName
+                                )}
+                            >
+                                {loading ? (
+                                    <FaSpinner
+                                        className="animate-spin"
+                                        color={loaderColor}
+                                        size={loaderSize}
+                                    />
+                                ) : (
+                                    children
+                                )}
+                            </p>
+                        </div>
+                    </button>
+                </Link>
+            );
+        }
+    }
+
+    if (span) {
+        return (
+            <span
+                {...props}
+                {...commonProps}
+                onClick={() => {
+                    if (disabled) return;
+                    if (onClick) onClick();
+                }}
+            >
+                {children}
+            </span>
+        );
+    }
+
+    return (
+        <button
+            type={type}
+            {...props}
+            disabled={commonProps.disabled}
+            id={commonProps.id}
+            className={cn(wide && 'w-full flex-grow h-auto focus:outline-none')}
+        >
+            <Ripples
+                id={commonProps.id}
+                className={cn(
+                    'hover:!shadow-none !shadow-none inline-block',
+                    commonProps.className
+                )}
+                onClick={onClick}
+                itemScope
+            >
+                <div
+                    className={cn(
+                        'text-clash-grotesk font-medium flex items-center justify-center space-x-2',
+                        childrenClassName
+                    )}
+                >
+                    {loading ? (
+                        <FaSpinner
+                            className="animate-spin"
+                            color={loaderColor}
+                            size={loaderSize}
+                        />
+                    ) : (
+                        children
+                    )}
+                </div>
+            </Ripples>
+        </button>
+    );
+};
+
+export default Button;
---
+++ components/molecules/Modal.js
@@ -0,0 +1,44 @@
+"use client";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { Cancel } from "@radix-ui/react-alert-dialog";
+import { cn } from "@/lib/utils";
+import { Inter_500 } from "@/lib/config/font.config";
+const Modal = ({ isOpen, onClose, children, title }) => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape" && isOpen && onClose) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
+  const modalContent = (
+    <div className="fixed inset-0 bg-[rgb(0,0,0,0.25)] flex items-center p-6 justify-center z-50 w-full ">
+      <div className="bg-white rounded-lg tablet:w-[434px] xxs:w-full shadow-lg">
+        <div className="flex justify-between items-center mb-2 px-8 py-2">
+          <h2 className={cn("text-lg ", Inter_500.className)}> {title}</h2>
+          <button
+            onClick={onClose}
+            className="text-gray-500 hover:text-gray-800 bg-gray4 rounded-full"
+          >
+            <Cancel />
+          </button>
+        </div>
+        <div className="border-b border-gray4"></div>
+        <div className="p-6 ">{children}</div>
+      </div>
+    </div>
+  );
+
+  return ReactDOM.createPortal(modalContent, document.body);
+};
+
+export default Modal;
