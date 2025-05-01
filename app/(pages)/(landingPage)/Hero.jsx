4de25374434767859e357ea1e5ca41fc80351a3c
Balogun Yusuf <yuslabibalogun2705@gmail.com>
2025-05-02 00:08:11 +0100

    add sign in

--- app/(pages)/(landingPage)/Hero.jsx
+++ app/(pages)/(landingPage)/Hero.jsx
@@ -5,30 +5,36 @@ import { poppins_600 } from "@/lib/config/font.config";
 import Button from "@/components/atoms/form/Button";
 
 const Hero = () => {
-    return (
-        <div className="flex flex-col items-center space-y-10 justify-center min-h-screen text-center bg-[#0d2615] text-white">
-            <h
-                className={cn(
-                    poppins_600,
-                    "text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4 leading-snug "
-                )}
-            >
-                Welcome to <span className=" bg-gradient-to-r from-green-400  via-green-500 to-green-600 text-transparent bg-clip-text">Deen Bridge</span>
-            </h>
+  return (
+    <div className="flex flex-col items-center space-y-10 justify-center min-h-screen text-center bg-[#0d2615] text-white">
+      <h
+        className={cn(
+          poppins_600,
+          "text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4 leading-snug "
+        )}
+      >
+        Welcome to{" "}
+        <span className=" bg-gradient-to-r from-green-400  via-green-500 to-green-600 text-transparent bg-clip-text">
+          Deen Bridge
+        </span>
+      </h>
 
-            <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-6 max-w-xl text-green-200">
-                Your journey to <span className="text-white font-semibold">excellence</span> starts here.
-            </p>
+      <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-6 max-w-xl text-green-200">
+        Your journey to{" "}
+        <span className="text-white font-semibold">excellence</span> starts
+        here.
+      </p>
 
-            <Button
-                round
-                outlined
-                className="bg-green-500 hover:bg-green-600 text-white px-10 py-3"
-            >
-                Sign Up
-            </Button>
-        </div>
-    );
+      <Button
+        to="/signup"
+        round
+        outlined
+        className="bg-green-500 hover:bg-green-600 text-white px-10 py-3"
+      >
+        Sign Up
+      </Button>
+    </div>
+  );
 };
 
 export default Hero;
