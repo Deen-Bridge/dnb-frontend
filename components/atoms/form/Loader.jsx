bfeb07a30ab9df1f4d8483ad67e485bf6498c26c
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2025-05-01 09:15:49 +0100

    Add Error component for displaying error messages

--- components/atoms/form/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Loader2Icon } from 'lucide-react';
-const Load = () => {
-    return (
-        <Loader2Icon className="animate-spin text-primary" size={24} />
-    )
-}
-
-export default Load;
