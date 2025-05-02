89f1821c7820a5d3caf8d01f8450616a2e5d035e
techifydev1 <yusufabdulqudus02@gmail.com>
2025-05-02 21:00:49 +0100

    Updated

--- components/organisms/auth/login-form.jsx
+++ components/organisms/auth/login-form.jsx
@@ -1,5 +1,5 @@
 'use client'
-
+import axios from "axios"
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -20,10 +20,10 @@ export function LoginForm({ className, ...props }) {
     e.preventDefault();
     setIsLoading(true);
     try {
-      // Simulate a login request
-      setTimeout(() => {
-        router.push("/")
-      }, 2000)
+      res = await axios.post("/api/auth/login", {
+        formData
+      });
+      console.log("Login successful", res.data);
     } catch (error) {
       console.error("Login failed", error);
     } finally {
